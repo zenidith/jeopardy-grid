b855ef04399b4f66b85f70ac9db442bec642327d
K Sheenan <kaynesheenan+git@gmail.com>
2021-11-17 01:52:44 +0900

    finished board - basic

--- app.js
+++ app.js
@@ -11,23 +11,27 @@
         using CSS Grid
 */
 
-async function getCategories() {
-    let response = await fetch("https://jservice.io/api/categories?count=4&offset=30")
+async function getCategories(count, offset) {
+    let response = await fetch(`https://jservice.io/api/categories?count=${count}&offset=${offset}`)
     let data = await response.json()
     return data
+}
+
+function getClueHtml(clueValue) {
+    return `<div class="my-category-clue" style="grid-row-start: ${clueValue / 100 + 1}">$${clueValue}</div>`
 }
 
 function getCategoryHtml(category) {
     return `
         <div class="my-category-title">${category.title}</div>
-        <div class="my-category-clue">$100</div>
-        <div class="my-category-clue">$200</div>
-        <div class="my-category-clue">$300</div>
-        <div class="my-category-clue">$400</div>
+        ${getClueHtml(100)}
+        ${getClueHtml(200)}
+        ${getClueHtml(300)}
+        ${getClueHtml(400)}
     `
 }
 
-getCategories().then(categories => {
+getCategories(5).then(categories => {
     console.log(categories)
     document.body.innerHTML = `<div class="board">
         ${categories.map(getCategoryHtml).join('')}
