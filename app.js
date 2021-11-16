eda1ec76fa27efe84186ff470a238ddbbecb5b1c
K Sheenan <kaynesheenan+git@gmail.com>
2021-11-16 15:45:24 +0900

    getCategoriesHMTL

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 async function getCategories() {
-    let response = await fetch("https://jservice.io/api/categories");
-    let data = await response.json();
-    return data;
+    let response = await fetch("https://jservice.io/api/categories?count=4&offset=30")
+    let data = await response.json()
+    return data
+}
+
+function getCategoryHtml(category) {
+    return `
+        <div class="my-category-title">${category.title}</div>
+    `
 }
 
 getCategories().then(categories => {
-    console.log(categories);
-    document.body.innerHTML = `<div class"board">
-    <div class="my-category-title">${categories[0].title}</div>
+    console.log(categories)
+    document.body.innerHTML = `<div class="board">
+        ${categories.map(getCategoryHtml).join('')}
     </div>`
 })
